d4da63ed91586237f01024df3fc2f30c4ed1c2ee
eyesofbrax <78063994+eyesofbrax@users.noreply.github.com>
2021-03-10 18:13:28 -0600

    Update Age.js

--- Age.js
+++ Age.js
@@ -2,9 +2,9 @@
      
      floatAge = parseFloat(prompt("Enter Age"));
                                
-     floatDays = parseFloat(floatAge/365.25).toFixed(2);
-     intWeeks = parseFloat(floatDays/7).toFixed(2);
+     floatDays = parseFloat(floatAge*365.25).toFixed(2);
+     intWeeks = parseInt(floatDays/7);
      floatMonths = parseFloat(floatAge*12).toFixed(2);
-     intFortnights = parseFloat(floatDays*14).toFixed(2);
+     intFortnights = parseInt(floatDays/14);
      
-     alert("You are currently = " + floatAge + "years old and you are " + floatDays + "days old and " + intWeeks + "weeks old and " + floatMonths "months old and " + intFortnights + "fortnights old");
+     alert("You are currently = " + floatAge + "years old and you are " + floatDays + "days old and " + intWeeks + "weeks old and " + floatMonths + "months old and " + intFortnights + "fortnights old");
